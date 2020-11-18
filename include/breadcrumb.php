d7aaf8ab50fcf2e77674943b24f109d1fbb1fa8d
Olivier <olivier.charlier.web@gmail.com>
2020-11-18 16:10:35 +0100

    fil d ariane

--- include/breadcrumb.php
+++ include/breadcrumb.php
@@ -3,9 +3,9 @@ $def = "index";
 $dPath = $_SERVER['PHP_SELF'];
 $dChunks = explode("/", $dPath);
 
-echo('<a class="dynNav" href="/">Accueil</a><span class="dynNav"> > </span>');
+echo('<a class="nav nav-item nav-link active" href="/">Accueil</a><span class="nav nav-item"> > </span>');
 for($i=1; $i<count($dChunks); $i++ ){
-	echo('<a class="dynNav" href="/');
+	echo('<a class="nav nav-item" href="/');
 	for($j=1; $j<=$i; $j++ ){
 		echo($dChunks[$j]);
 		if($j!=count($dChunks)-1){ echo("/");}
@@ -16,7 +16,7 @@ for($i=1; $i<count($dChunks); $i++ ){
 		if ($prChunks[0] == $def) $prChunks[0] = "";
 		$prChunks[0] = $prChunks[0] . "</a>";
 	}
-	else $prChunks[0]=$dChunks[$i] . '</a><span class="dynNav"> > </span>';
+	else $prChunks[0]=$dChunks[$i] . '</a><span class="nav nav-item"> > </span>';
 	echo('">');
 	echo(str_replace("_" , " " , $prChunks[0]));
 } 
