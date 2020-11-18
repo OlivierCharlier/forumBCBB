80942323d00bddc296b5be0e253377c040e7b244
Bertrand2 <dossogne.bertrand@gmail.com>
2020-11-18 16:02:15 +0100

    page split

---
+++ include/categories.php
@@ -0,0 +1,181 @@
+<div class="container-fluid col-12 col-md-9">
+
+<!--  START OF CATEGORY ONE  -->
+<h1>Administration</h1>
+<div class="row">
+  <section class="col-12 col-md-4">
+	<div class="card mb-3" style="max-width: 540px;">
+	  <div class="row no-gutters">
+		<div class="col-md-4">
+		  <img src="..." class="card-img" alt="...">
+		</div>
+		<div class="col-md-8">
+		  <div class="card-body">
+			<h5 class="card-title">Card title</h5>
+			<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+			<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+		  </div>
+		</div>
+	  </div>
+	</div>
+  </section>
+  <section class="col-12 col-md-4">
+	<div class="card mb-3" style="max-width: 540px;">
+	  <div class="row no-gutters">
+		<div class="col-md-4">
+		  <img src="..." class="card-img" alt="...">
+		</div>
+		<div class="col-md-8">
+		  <div class="card-body">
+			<h5 class="card-title">Card title</h5>
+			<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+			<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+		  </div>
+		</div>
+	  </div>
+	</div>
+  </section>
+  <section class="col-12 col-md-4">
+	<div class="card mb-3" style="max-width: 540px;">
+	  <div class="row no-gutters">
+		<div class="col-md-4">
+		  <img src="..." class="card-img" alt="...">
+		</div>
+		<div class="col-md-8">
+		  <div class="card-body">
+			<h5 class="card-title">Card title</h5>
+			<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+			<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+		  </div>
+		</div>
+	  </div>
+	</div>
+  </section>
+  <section class="col-12 col-md-4">
+	<div class="card mb-3" style="max-width: 540px;">
+	  <div class="row no-gutters">
+		<div class="col-md-4">
+		  <img src="..." class="card-img" alt="...">
+		</div>
+		<div class="col-md-8">
+		  <div class="card-body">
+			<h5 class="card-title">Card title</h5>
+			<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+			<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+		  </div>
+		</div>
+	  </div>
+	</div>
+  </section>
+  <section class="col-12 col-md-4">
+	<div class="card mb-3" style="max-width: 540px;">
+	  <div class="row no-gutters">
+		<div class="col-md-4">
+		  <img src="..." class="card-img" alt="...">
+		</div>
+		<div class="col-md-8">
+		  <div class="card-body">
+			<h5 class="card-title">Card title</h5>
+			<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+			<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+		  </div>
+		</div>
+	  </div>
+	</div>
+  </section>
+</div>
+<!-- END OF CATEGORY ONE -->
+
+<!--  START OF CATEGORY TWO  -->
+<h1>Entrainements</h1>
+<div class="row">
+  <section class="col-12 col-md-4">
+	<div class="card mb-3" style="max-width: 540px;">
+	  <div class="row no-gutters">
+		<div class="col-md-4">
+		  <img src="..." class="card-img" alt="...">
+		</div>
+		<div class="col-md-8">
+		  <div class="card-body">
+			<h5 class="card-title">Card title</h5>
+			<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+			<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+		  </div>
+		</div>
+	  </div>
+	</div>
+  </section>
+  <section class="col-12 col-md-4">
+	<div class="card mb-3" style="max-width: 540px;">
+	  <div class="row no-gutters">
+		<div class="col-md-4">
+		  <img src="..." class="card-img" alt="...">
+		</div>
+		<div class="col-md-8">
+		  <div class="card-body">
+			<h5 class="card-title">Card title</h5>
+			<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+			<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+		  </div>
+		</div>
+	  </div>
+	</div>
+  </section>
+</div>
+<!-- END OF CATEGORY TWO -->
+
+<!--  START OF CATEGORY THREE  -->
+<h1>Materiel</h1>
+<div class="row">
+  <section class="col-12 col-md-4">
+	<div class="card mb-3" style="max-width: 540px;">
+	  <div class="row no-gutters">
+		<div class="col-md-4">
+		  <img src="..." class="card-img" alt="...">
+		</div>
+		<div class="col-md-8">
+		  <div class="card-body">
+			<h5 class="card-title">Card title</h5>
+			<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+			<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+		  </div>
+		</div>
+	  </div>
+	</div>
+  </section>
+  <section class="col-12 col-md-4">
+	<div class="card mb-3" style="max-width: 540px;">
+	  <div class="row no-gutters">
+		<div class="col-md-4">
+		  <img src="..." class="card-img" alt="...">
+		</div>
+		<div class="col-md-8">
+		  <div class="card-body">
+			<h5 class="card-title">Card title</h5>
+			<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+			<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+		  </div>
+		</div>
+	  </div>
+	</div>
+  </section>
+  <section class="col-12 col-md-4">
+	<div class="card mb-3" style="max-width: 540px;">
+	  <div class="row no-gutters">
+		<div class="col-md-4">
+		  <img src="..." class="card-img" alt="...">
+		</div>
+		<div class="col-md-8">
+		  <div class="card-body">
+			<h5 class="card-title">Card title</h5>
+			<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+			<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+		  </div>
+		</div>
+	  </div>
+	</div>
+  </section>
+</div>
+<!-- END OF CATEGORY THREE -->
+
+</div>
