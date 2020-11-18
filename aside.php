17ca518d311bec1aa436b53b50218cf2d239ddbf
Daems Aline <daems.aline90@gmail.com>
2020-11-18 14:49:26 +0100

    html frontpage

---
+++ aside.php
@@ -0,0 +1,94 @@
+
+    <div class="col-xs-12 col-sn-12 col-md-3 col-lg-3">
+
+                    <!-----------SEARCH-------------->
+                <form>
+                  <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                  <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+                    <!-----------USERNAME-------------->
+                <form>
+                  </br>
+                  <p> Login - Register</p>
+                  <div class="col-auto">
+                    <label class="sr-only" for="inlineFormInputGroup">Username</label>
+                    <div class="input-group mb-2">
+                      <div class="input-group-prepend">
+                      <div class="input-group-text">@
+                      </div>
+                      </div>
+                      <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="Username">
+                    </div>
+                </form> 
+                    <!-----------PASSWORD-------------->
+                <form>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1">
+                  </div>
+                  <div class="form-group form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Remember me</label>
+                  </div>
+                  <button type="submit" class="btn btn-primary">Login</button>
+                </form>
+                </br>
+         
+
+                  <!-----------LAST POSTS -------------->
+            <div class="card bg-light mb-3" style="max-width: 18rem;">
+                <div class="card-header">Last pots
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Post-catogry3</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <h5 class="card-title">Post-catogry3</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <h5 class="card-title">Post-catogry3</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <h5 class="card-title">Post-catogry3</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+          </div>
+
+                <!-----------LAST ACTIVE USERS -------------->
+                <section class="card bg-light mb-3" style="max-width: 18rem;">
+                  <div class="card-header">Last Active Users
+                  </div>
+                  <div class="card-body">
+                        <div class="media">
+                            <a class="pull-left" href="#">
+                                <img class="media-object dp img-circle" src="https://www.flaticon.com/svg/static/icons/svg/2317/2317933.svg" style="width: 50px;height:50px;">
+                            </a>
+                            <div class="media-body">
+                                <h5 class="media-heading">Truc muche 
+                                </br>
+                                <small> I love badminiton</small></h5>
+                            </div>    
+                     </div>
+
+                      <div class="media">
+                        <a class="pull-left" href="#">
+                            <img class="media-object dp img-circle" src="https://www.flaticon.com/svg/static/icons/svg/2761/2761758.svg" style="width: 50px;height:50px;">
+                        </a>
+                        <div class="media-body">
+                            <h5 class="media-heading">Pelotte Lamie
+                            </br>
+                            <small>Help</small></h5>
+                        </div>  
+                      </div>
+
+                      <div class="media">
+                        <a class="pull-left" href="#">
+                            <img class="media-object dp img-circle" src="https://www.flaticon.com/svg/static/icons/svg/731/731870.svg" style="width: 50px;height:50x;">
+                        </a>
+                        <div class="media-body">
+                            <h5 class="media-heading">Raquette Man
+                            </br>
+                            <small>New</small></h5>
+                        </div>  
+                      </div>
+                </section>
+    </div>
+
+
---
+++ index.php
@@ -0,0 +1,418 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js"></script>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet"> 
+    <title> Badminton Club Bain de Bretagne.  </title>
+</head>
+<body>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+<!-- MAIN IMAGE  -->
+  <div class="image">
+
+
+
+<!-- TEXT MAIN IMAGE -->
+    <h1 class="Title"> BADMINTON CLUB BAIN DE BRETAGNE </h1>
+
+    <a href="#"> Register </a> 
+    <a href="#"> Login </a> 
+
+  </div>
+
+    <!-- CONTENT -->
+
+    <!-- START OF WHITE BACKGROUND  -->
+  <div> 
+
+    <!-- nav barre  -->
+
+    <ul class="nav">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Board Index</a>
+      </li>
+    </ul>
+
+    <!--  START OF CATEGORY ONE  -->
+    <h1>Administration</h1>
+
+    <!-- S'adapte sur tout l'écran-->
+    <div class="container-fluid">
+
+      <!-- marge auto à droite et gauche-->
+      <div class="container-fluid row align-items-start">
+      
+        <!-- LIGNE 1-->
+        <div class="row col-12 col-md-9">
+
+          <!-- CARD 1  TOPIC TYPE DEMOS -->
+
+          <section class="col-12 col-sn-12 col-md-4 col-lg-4">
+            <div class="card mb-3" style="max-width: 540px;">
+              <div class="row no-gutters">
+                <div class="col-md-4">
+                  <img src="..." class="card-img" alt="...">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <!-- CARD 2 TOPIC ICON DEMOS  -->
+      
+          <section class="col-12 col-sn-12 col-md-4 col-lg-4">
+            <div class="card mb-3" style="max-width: 540px;">
+              <div class="row no-gutters">
+                <div class="col-md-4">
+                  <img src="..." class="card-img" alt="...">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <!-- CARD 3 UNREAD FORUM -->
+      
+          <section class="col-12 col-sn-12 col-md-4 col-lg-4">
+            <div class="card mb-3" style="max-width: 540px;">
+              <div class="row no-gutters">
+                <div class="col-md-4">
+                  <img src="..." class="card-img" alt="...">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+      
+
+
+          <!-- LIGNE 2-->
+
+
+          <!-- CARD 4 READ FORUM -->
+        
+          <section class="col-12 col-sn-12 col-md-4 col-lg-4">
+            <div class="card mb-3" style="max-width: 540px;">
+              <div class="row no-gutters">
+                <div class="col-md-4">
+                  <img src="..." class="card-img" alt="...">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <!-- CARD 5 A FORUM WITH SOME SUBFORUMS -->
+
+          <section class="col-12 col-sn-12 col-md-4 col-lg-4">
+            <div class="card mb-3" style="max-width: 540px;">
+              <div class="row no-gutters">
+                <div class="col-md-4">
+                  <img src="..." class="card-img" alt="...">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+      
+        </div> <!--fermeture de div row -->
+
+        
+
+
+
+
+
+      <!-- END OF CATEGORY ONE -->
+
+
+
+
+    <!--  START OF CATEGORY TWO  -->
+    <h1>Entrainements</h1>
+
+ 
+    
+
+    <!-- LIGNE 1-->
+        <div class="row">
+
+<!-- CARD 1  Locked forum with Unread topics  -->
+      
+          <section class="col-12 col-sn-12 col-md-4 col-lg-4">
+            <div class="card mb-3" style="max-width: 540px;">
+              <div class="row no-gutters">
+                <div class="col-md-4">
+                  <img src="..." class="card-img" alt="...">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+
+            <!-- CARD 2 Locked forum with read Topics -->
+
+          <section class="col-12 col-sn-12 col-md-4 col-lg-4">
+            <div class="card mb-3" style="max-width: 540px;">
+              <div class="row no-gutters">
+                <div class="col-md-4">
+                  <img src="..." class="card-img" alt="...">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+
+  
+        </div> <!-- fermeture de div row -->
+
+     
+
+
+      <!-- END OF CATEGORY TWO -->
+
+      <!--  START OF CATEGORY THREE  -->
+    <h1>Materiel</h1>
+
+<!-- S'adapte sur tout l'écran-->
+      <div class="container-fluid">
+  
+  <!-- marge auto à droite et gauche-->
+      <div class="container">
+    
+
+    <!-- LIGNE 1-->
+        <div class="row">
+
+<!-- CARD 1  This is a Linked Forum  -->
+      
+          <section class="col-12 col-sn-12 col-md-4 col-lg-4">
+            <div class="card mb-3" style="max-width: 540px;">
+              <div class="row no-gutters">
+                <div class="col-md-4">
+                  <img src="..." class="card-img" alt="...">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+
+    <!-- CARD 2 Password Protected forum -->
+
+          <section class="col-12 col-sn-12 col-md-4 col-lg-4">
+            <div class="card mb-3" style="max-width: 540px;">
+              <div class="row no-gutters">
+                <div class="col-md-4">
+                  <img src="..." class="card-img" alt="...">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+
+    <!-- CARD 3 LAST TEST WITH A SUBFORUM -->
+
+          <section class="col-12 col-sn-12 col-md-4 col-lg-4">
+            <div class="card mb-3" style="max-width: 540px;">
+              <div class="row no-gutters">
+                <div class="col-md-4">
+                  <img src="..." class="card-img" alt="...">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+
+
+
+        </div> <!-- fermeture de div row -->
+
+     
+
+<!-- END OF CATEGORY THREE -->
+      
+  </div>  <!--fermeture de la div principale du white background-->
+
+
+  </div><!-- fermerture de container  -->
+
+</div> <!--fermeture de container fluid -->
+ <!-- START OF ASIDE -->
+ <?php include("aside.php"); ?>
+
+<!-- END OF ASIDE -->
+
+
+
+<!-- END OF WHITE BACKGROUND -->
+
+
+
+<!-- START OF FOOTER  -->
+
+  <!-- GRADIENT BACKGROUND -->
+
+  <div>
+
+  <!-- S'adapte sur tout l'écran-->
+    <div class="container-fluid">
+  
+    <!-- marge auto à droite et gauche-->
+      <div class="container">
+      
+
+      <!-- LIGNE 1-->
+      <div class="row">
+
+        <!-- ICON TWITTER -->
+
+        <a href="#"> <i class="fab fa-twitter"></i> </a>
+
+        <!-- ICON APPLE -->
+
+        <a href="#"> <i class="fab fa-apple"></i></a>
+
+
+        <!-- ICON FACEBOOK -->
+
+
+
+        <a href="#"> <i class="fab fa-facebook-f"></i></a>
+
+        <!-- ICON CODEPEN -->
+
+        <a href="#"><i class="fab fa-codepen"></i></a>
+
+        <!-- ICON GOOGLE + -->
+
+        <a href="#"><i class="fab fa-google-plus-g"></i></a>
+
+        <!-- ICON DIGG  -->
+
+        <a href="#"><i class="fab fa-digg"></i></a>
+
+
+        <!-- ICON PRINTEREST  -->
+
+        <a href="#"><i class="fab fa-pinterest-p"></i></a>
+
+  </div>
+    </div>
+      </div>
+        </div>
+
+
+<!-- END OF BACKGROUND GRADIENT  -->
+
+<!-- START OF GREY BACKGROUND  -->
+
+  <div>
+
+    <!-- CONTACT -->
+
+    <a href="#"> <i class="fas fa-envelope"></i> Contact us </a>
+
+    <!-- TEAM -->
+
+    <a href="#">  <i class="fas fa-shield-alt"></i> The team </a>
+
+    <!-- TERMS -->
+
+    <a href="#"> Terms <i class="fas fa-check"></i></a>
+
+    <!-- PRIVACY  -->
+
+    <a href="#"> <i class="fas fa-lock"></i> Privacy </a>
+
+    <!-- MEMBERS -->
+
+    <a href="#"><i class="fas fa-user"></i> Members  </a>
+
+    <!-- DELETE MEMBERS -->
+
+    <a href="#"> <i class="far fa-trash-alt"></i> Delete members </a>
+
+    <!-- UTC  -->
+
+    <p> All times are UTC </p>
+
+
+  </div>
+
+<!-- END OF GREY BACKRGOUND  -->
+
+<!-- END OF FOOTER -->
+
+
+</body>
+</html>
