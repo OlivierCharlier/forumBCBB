80769546f0df44b6cee990736b93c06b610676aa
Olivier <olivier.charlier.web@gmail.com>
2020-11-23 15:54:37 +0100

    bdd online (update comment)

--- include/bdd.php
+++ include/bdd.php
@@ -1,6 +1,6 @@
 
   <?php
-  // connection to the base (local)
+  // connection to the base (db4free.net)
   $bdd = new PDO('mysql:host=db4free.net;dbname=badmintonclubbb;charset=utf8', 'genesis', 'TheMusicalBox', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
   ?>
 
