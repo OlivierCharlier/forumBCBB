a7b3f5bbb95a8176a07cc9b1f7fe8234a963c1b3
Bertrand2 <dossogne.bertrand@gmail.com>
2020-11-23 12:26:33 +0100

    update boards.php

--- include/boards.php
+++ include/boards.php
@@ -1,184 +1,64 @@
-
-
 <div class="container-fluid col-12 col-md-9">
 
-<!--  START OF CATEGORY ONE  -->
-<h1>Board 1</h1>
-<div class="row">
-  <section class="col-12 col-md-4">
-	<div class="card mb-3" style="max-width: 540px;">
-	  <div class="row no-gutters">
-		<div class="col-md-4">
-		  <img src="..." class="card-img" alt="...">
+	<?php
+		$boardQuery = "SELECT * FROM boards";
+		$boardResult = mysqli_query($bdd, $boardQuery);
+		while ($boardRow = mysqli_fetch_assoc($boardResult)) {
+	?>
+		<h2><?= $boardRow["boardsName"]; ?></h2>
+		<div class="row">
+		<?php
+			$forumQuery = "SELECT * FROM forums WHERE forumBoardId = " . $boardRow["boardsId"];
+			$forumResult = mysqli_query($bdd, $forumQuery);
+			while ($forumRow = mysqli_fetch_assoc($forumResult)) {
+				/* SELECTING LAST TOPIC OF THE FORUM */
+				$lastTopicQuery = "SELECT topicCreationDate FROM topics WHERE topicForumId = " . $forumRow["forumId"] . " ORDER BY topicId DESC LIMIT 1";
+				$lastTopicResult = mysqli_query($bdd, $lastTopicQuery);
+				$lastTopic = mysqli_fetch_assoc($lastTopicResult);
+				/* SELECTING THE TOTAL OF POSTS OF THE FORUM */
+				$totalTopicQuery = "SELECT COUNT(*) AS 'total' FROM topics WHERE topicForumId = " . $forumRow["forumId"];
+				$totalTopicResult = mysqli_query($bdd, $totalTopicQuery);
+				$totalTopicRow = mysqli_fetch_assoc($totalTopicResult);
+		?>
+		<section class="col-12 col-md-4" style="max-width: 540px;">
+			<div class="card mb-3">
+				<div class="card-body">
+					<div class="row no-gutters">
+						<div class="col-3">
+							<img src="..." class="card-img" alt="...">
+						</div>
+						<div class="col-9">
+							<h5 class="card-title"><?= $forumRow["forumName"]; ?></h5>
+							<p class="card-text"><?= $forumRow["forumDesc"]; ?></p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="mr-2">
+							<!-- TODO -->
+							<p><?= $totalTopicRow["total"]; ?></p>
+							<p class="text-muted">Topics</p>
+						</div>
+						<div class="mr-2">
+							<!-- TODO -->
+							<p>908</p>
+							<p class="text-muted">Posts</p>
+						</div>
+						<div class="mr-2">
+							<!-- TODO -->
+							<p><?= $lastTopic["topicCreationDate"]; ?></p>
+							<p class="text-muted">Last post</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<?php        
+			}
+		?>
 		</div>
-		<div class="col-md-8">
-		  <div class="card-body">
-			<h5 class="card-title">Forum 1</h5>
-			<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-			<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-		  </div>
-		</div>
-	  </div>
-	</div>
-  </section>
-  <section class="col-12 col-md-4">
-	<div class="card mb-3" style="max-width: 540px;">
-	  <div class="row no-gutters">
-		<div class="col-md-4">
-		  <img src="..." class="card-img" alt="...">
-		</div>
-		<div class="col-md-8">
-		  <div class="card-body">
-			<h5 class="card-title">Forum 2</h5>
-			<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-			<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-		  </div>
-		</div>
-	  </div>
-	</div>
-  </section>
-  <section class="col-12 col-md-4">
-	<div class="card mb-3" style="max-width: 540px;">
-	  <div class="row no-gutters">
-		<div class="col-md-4">
-		  <img src="..." class="card-img" alt="...">
-		</div>
-		<div class="col-md-8">
-		  <div class="card-body">
-			<h5 class="card-title">Forum 3</h5>
-			<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-			<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-		  </div>
-		</div>
-	  </div>
-	</div>
-  </section>
-  <section class="col-12 col-md-4">
-	<div class="card mb-3" style="max-width: 540px;">
-	  <div class="row no-gutters">
-		<div class="col-md-4">
-		  <img src="..." class="card-img" alt="...">
-		</div>
-		<div class="col-md-8">
-		  <div class="card-body">
-			<h5 class="card-title">Forum 4</h5>
-			<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-			<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-		  </div>
-		</div>
-	  </div>
-	</div>
-  </section>
-  <section class="col-12 col-md-4">
-	<div class="card mb-3" style="max-width: 540px;">
-	  <div class="row no-gutters">
-		<div class="col-md-4">
-		  <img src="..." class="card-img" alt="...">
-		</div>
-		<div class="col-md-8">
-		  <div class="card-body">
-			<h5 class="card-title">Forum 5</h5>
-			<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-			<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-		  </div>
-		</div>
-	  </div>
-	</div>
-  </section>
-</div>
-<!-- END OF CATEGORY ONE -->
-
-<!--  START OF CATEGORY TWO  -->
-<h1>Board 2</h1>
-<div class="row">
-  <section class="col-12 col-md-4">
-	<div class="card mb-3" style="max-width: 540px;">
-	  <div class="row no-gutters">
-		<div class="col-md-4">
-		  <img src="..." class="card-img" alt="...">
-		</div>
-		<div class="col-md-8">
-		  <div class="card-body">
-			<h5 class="card-title">Forum 1</h5>
-			<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-			<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-		  </div>
-		</div>
-	  </div>
-	</div>
-  </section>
-  <section class="col-12 col-md-4">
-	<div class="card mb-3" style="max-width: 540px;">
-	  <div class="row no-gutters">
-		<div class="col-md-4">
-		  <img src="..." class="card-img" alt="...">
-		</div>
-		<div class="col-md-8">
-		  <div class="card-body">
-			<h5 class="card-title">Forum 2</h5>
-			<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-			<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-		  </div>
-		</div>
-	  </div>
-	</div>
-  </section>
-</div>
-<!-- END OF CATEGORY TWO -->
-
-<!--  START OF CATEGORY THREE  -->
-<h1>Board 3</h1>
-<div class="row">
-  <section class="col-12 col-md-4">
-	<div class="card mb-3" style="max-width: 540px;">
-	  <div class="row no-gutters">
-		<div class="col-md-4">
-		  <img src="..." class="card-img" alt="...">
-		</div>
-		<div class="col-md-8">
-		  <div class="card-body">
-			<h5 class="card-title">Forum 1</h5>
-			<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-			<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-		  </div>
-		</div>
-	  </div>
-	</div>
-  </section>
-  <section class="col-12 col-md-4">
-	<div class="card mb-3" style="max-width: 540px;">
-	  <div class="row no-gutters">
-		<div class="col-md-4">
-		  <img src="..." class="card-img" alt="...">
-		</div>
-		<div class="col-md-8">
-		  <div class="card-body">
-			<h5 class="card-title">Forum 2</h5>
-			<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-			<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-		  </div>
-		</div>
-	  </div>
-	</div>
-  </section>
-  <section class="col-12 col-md-4">
-	<div class="card mb-3" style="max-width: 540px;">
-	  <div class="row no-gutters">
-		<div class="col-md-4">
-		  <img src="..." class="card-img" alt="...">
-		</div>
-		<div class="col-md-8">
-		  <div class="card-body">
-			<h5 class="card-title">Forum 3</h5>
-			<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-			<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-		  </div>
-		</div>
-	  </div>
-	</div>
-  </section>
-</div>
-<!-- END OF CATEGORY THREE -->
+	<?php        
+		}
+	?>
 
 </div>
 
