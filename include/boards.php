c6f4aa95d07d551c61d137326b1c29316623bde7
Bertrand2 <dossogne.bertrand@gmail.com>
2020-11-24 10:29:52 +0100

    boards.php modifications

--- include/boards.php
+++ include/boards.php
@@ -11,21 +11,33 @@
 			$forumQuery = "SELECT * FROM forums WHERE forumBoardId = " . $boardRow["boardsId"];
 			$forumResult = mysqli_query($bdd, $forumQuery);
 			while ($forumRow = mysqli_fetch_assoc($forumResult)) {
+
 				/* SELECTING LAST TOPIC OF THE FORUM */
-				$lastTopicQuery = "SELECT topicCreationDate FROM topics WHERE topicForumId = " . $forumRow["forumId"] . " ORDER BY topicId DESC LIMIT 1";
-				$lastTopicResult = mysqli_query($bdd, $lastTopicQuery);
-				$lastTopic = mysqli_fetch_assoc($lastTopicResult);
+				$lastTopicQuery = "SELECT topicCreationDate FROM topics WHERE topicForumId = ? ORDER BY topicId DESC LIMIT 1";
+				$lastTopicResult = mysqli_prepare($bdd, $lastTopicQuery);
+				mysqli_stmt_bind_param($lastTopicResult, "s", $forumRow["forumId"]);
+				mysqli_stmt_execute($lastTopicResult);
+				mysqli_stmt_bind_result($lastTopicResult, $lastTopic);
+				mysqli_stmt_fetch($lastTopicResult);
+				mysqli_stmt_close($lastTopicResult);
+				/*$lastTopicResult = mysqli_query($bdd, $lastTopicQuery);*/
+				/* $lastTopic = mysqli_fetch_assoc($lastTopicResult); */
+
 				/* SELECTING THE TOTAL OF POSTS OF THE FORUM */
-				$totalTopicQuery = "SELECT COUNT(*) AS 'total' FROM topics WHERE topicForumId = " . $forumRow["forumId"];
-				$totalTopicResult = mysqli_query($bdd, $totalTopicQuery);
-				$totalTopicRow = mysqli_fetch_assoc($totalTopicResult);
+				$totalTopicQuery = "SELECT COUNT(*) AS 'total' FROM topics WHERE topicForumId = ?";
+				$totalTopicResult = mysqli_prepare($bdd, $totalTopicQuery);
+				mysqli_stmt_bind_param($totalTopicResult, "s", $forumRow["forumId"]);
+				mysqli_stmt_execute($totalTopicResult);
+				mysqli_stmt_bind_result($totalTopicResult, $totalTopic);
+				mysqli_stmt_fetch($totalTopicResult);
+				mysqli_stmt_close($totalTopicResult);
 		?>
 		<section class="col-12 col-md-4" style="max-width: 540px;">
 			<div class="card mb-3">
-				<div class="card-body">
+				<a href="forum.php?id=<?= $forumRow["forumId"]; ?>" class="card-body">
 					<div class="row no-gutters">
 						<div class="col-3">
-							<img src="..." class="card-img" alt="...">
+							<img src="pictures/<?= $forumRow["forumPicSrc"]; ?>" class="card-img" alt="...">
 						</div>
 						<div class="col-9">
 							<h5 class="card-title"><?= $forumRow["forumName"]; ?></h5>
@@ -35,7 +47,7 @@
 					<div class="row">
 						<div class="mr-2">
 							<!-- TODO -->
-							<p><?= $totalTopicRow["total"]; ?></p>
+							<p><?= $totalTopic; ?></p>
 							<p class="text-muted">Topics</p>
 						</div>
 						<div class="mr-2">
@@ -45,11 +57,11 @@
 						</div>
 						<div class="mr-2">
 							<!-- TODO -->
-							<p><?= $lastTopic["topicCreationDate"]; ?></p>
+							<p><?= $lastTopic; ?></p>
 							<p class="text-muted">Last post</p>
 						</div>
 					</div>
-				</div>
+				</a>
 			</div>
 		</section>
 		<?php        
@@ -61,4 +73,5 @@
 	?>
 
 </div>
+
 
