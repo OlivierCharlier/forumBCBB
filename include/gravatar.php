96f06a219780b3d6e1dc53b9a46389dfcd8895aa
Olivier <olivier.charlier.web@gmail.com>
2020-11-25 14:14:38 +0100

    gravatar

--- include/gravatar.php
+++ include/gravatar.php
@@ -6,31 +6,13 @@
  * @param string $email E-mail rattaché au gravatar
  * @param int $size Dimension du gravatar
  * @return string $out Code HTML du gravatar
- * @author Sithran
  */
-function gravatar($email, $size=60)
-{
-  // Définition des paramètres utiles
-  $default = urlencode('https://cdn1.iconfinder.com/data/icons/sport-avatar-7/64/05-sports-badminton-sport-avatar-player-512.png');
-  $email = md5($email);
-  // Détermination de l'url paramétrée
-  $url = 'http://www.gravatar.com/avatar.php';
-  $url.= '?gravatar_id=%s';
-  $url.= '&amp;size=%d';
-  $url.= '&amp;default=%s';
-  // Création de l'url
-  $url = sprintf
-  (
-    $url,
-    $email,
-    intval($size),
-    $default
-  );
-  // Génération de la sortie HTML
-  $out = '<img src="'. $url .'" alt="Gravatar" title="Gravatar" />',
-  return $out;
-}
 
-echo gravatar('olivier420@gmail.com', 55);
-
+ // info : http://fr.gravatar.com/site/implement/images/php/
+$email = $_SESSION['userEmail'];
+$default = "https://cdn1.iconfinder.com/data/icons/sport-avatar-7/64/05-sports-badminton-sport-avatar-player-512.png";
+$size = 120;
+$grav_url = "https://www.gravatar.com/avatar/" . md5( strtolower( trim( $email ) ) ) . "?d=" . urlencode( $default ) . "&s=" . $size;
 ?>
+
+<img class="avatar" src="<?php echo $grav_url; ?>" alt="picture" />
