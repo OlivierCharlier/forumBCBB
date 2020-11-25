f90a170205d1ff6640a4786263e4de0f6a2d7880
Olivier <olivier.charlier.web@gmail.com>
2020-11-25 14:50:05 +0100

    register link into the header

--- include/header.php
+++ include/header.php
@@ -3,7 +3,7 @@
 <!-- TEXT MAIN IMAGE -->
 <h1 class="Title"> BADMINTON CLUB BAIN DE BRETAGNE </h1>
 <div class="logcontainer">
-<a href="#" class="login"> <i class="far fa-arrow-alt-circle-right"></i> Register </a> 
+<a href="register.php" class="login"> <i class="far fa-arrow-alt-circle-right"></i> Register </a> 
 <a href="#" class="login"> <i class="far fa-user"></i> Login </a> 
 </div>
 </div>
