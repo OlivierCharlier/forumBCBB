5f1992d19b97d0d1c8c31fc7c552c509b7b72c1f
Bertrand2 <dossogne.bertrand@gmail.com>
2020-11-26 14:49:48 +0100

    post creator

---
+++ include/postCreator.php
@@ -0,0 +1,37 @@
+<div class="container-fluid col-12 col-md-9 p-5">
+
+	<p>
+		Forum Rules
+	</p>
+
+	<?php 
+		$topicId = $_GET["id"];
+		$postErrorMessage=" ";
+
+		if(isset($_POST["postCreation"])){
+
+			$message = htmlspecialchars($_POST['message']);
+
+			if(empty($message)) {
+				$postErrorMessage = "You must write a message !";
+			} else {
+				$newPostQuery = "INSERT INTO posts (postUserId, postTopicId, postContent) VALUES (?, ?, ?)";
+				$newPostResult = $bdd->prepare($newPostQuery);
+				$newPostResult->execute([$_SESSION["userId"], $topicId, $message]);
+
+				unset($message);
+				header("Location: posts.php?id=$topicId");
+			}
+
+		}
+	?>
+
+	<p class="text-danger"><?= $postErrorMessage?></p>
+	<form method="post" class="d-flex flex-column p-3">
+		<label for="message" class="p-2 mt-4">Message</label>
+		<textarea maxlength="2000" id="topicMessage" name="message"></textarea>
+	
+		<button type="submit" name="postCreation" id="postCreation" class="btn-success rounded-pill w-25 m-3 mt-4 align-self-center">Create new topic</button>
+	</form>
+
+</div>
---
+++ newPost.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php include("include/head.php"); ?>
+</head>
+<body>
+
+  <!-- HEADER  -->
+  <?php include("include/header.php"); ?>
+
+  <!-- START OF PAGE CONTENT  -->
+  <main class="background">
+
+    <!-- NAV BAR  -->
+    <?php include("include/breadcrumb.php"); ?>
+    <!-- S'adapte sur tout l'écran-->
+    <div class="container-fluid row align-items-start">
+
+      <!-- CATEGORIES -->
+      <?php include("include/postCreator.php"); ?>
+
+      <!-- aside -->
+      <?php include("include/aside.php"); ?>
+    </div>
+
+  </main>
+  <!-- END OF PAGE CONTENT -->
+
+
+  <!-- FOOTER  -->
+  <?php include("include/footer.php"); ?>
+
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+</html>
